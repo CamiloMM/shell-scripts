849537e508de3dea159704b19356519ad85b410c
CamiloMM <herzschaltung@yahoo.com.ar>
2015-02-07 23:03:28 -0200

    Fixed several bugs with google-translate.sh, it's kicking ass now.

--- google-translate.sh
+++ google-translate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # syntax:  google-translate.sh SOURCE TARGET PHRASE
 # example: google-translate.sh auto pt-BR 'Spaghetti alla puttanesca means spaghetti of the whore'
-SL="$1"
-SD="$2"
+source="$1"
+dest="$2"
 shift 2
 encodeURIComponent() { perl -pe 's/([^a-zA-Z0-9_.!~*()'\''-])/sprintf("%%%02X", ord($1))/ge'; }
-CONTENT="$(printf %s "$@" | encodeURIComponent)"
-TEXT="$(curl -s -A "Mozilla" "https://translate.google.com/translate_a/single?client=t&sl=auto&hl=en&dt=t&ie=UTF-8&oe=UTF-8&q=$CONTENT" | awk -F'"' '{print $2}')"
-echo "$TEXT"
+original="$(printf %s "$@" | encodeURIComponent)"
+json="$(curl -s -A "Mozilla" "https://translate.google.com/translate_a/single?client=t&sl=$source&hl=$dest&dt=t&ie=UTF-8&oe=UTF-8&q=$original")"
+# Now this may seem too much work because we could use sed/awk/etc. instead, but besides being "the right thing"
+# to parse JSON correctly, Google Translate also separates phrases, so this is a no-brainer. Besides I love js.
+node -e "process.stdout.write(($json)[0].map(function(i){return i[0]}).join(''))"
