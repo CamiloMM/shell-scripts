a6632d99fa8038477a0aea19aac9aec6a8b681da
CamiloMM <herzschaltung@yahoo.com.ar>
2015-02-11 10:36:46 -0200

    Add xmlentities as a separate script.

---
+++ xmlentities.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Inserts XML entities into raw text from STDIN and writes it to STDOUT.
+sed -r 's/\\/\\\\/g;s/&/\&amp;/g;s/"/\&quot;/g;s/'\''/\&apos;/g;s/</\&lt;/g;s/>/\&gt;/g' \
+| iconv -f UTF-8 -t JAVA | sed -r 's/\\u([0-9a-f]{4})/\&#x\1;/g;s/\\&#x([0-9a-f]{4});/\\u\1/g'; }
