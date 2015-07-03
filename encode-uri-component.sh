9385de4cc879fc0704796deec86c5fc4955602bc
CamiloMM <herzschaltung@yahoo.com.ar>
2015-07-02 22:45:20 -0300

    Now you can pass params as shell arguments or stdin

--- encode-uri-component.sh
+++ encode-uri-component.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # This simple script encodes whatever is given to it from stdin as
 # an URI component (e.g., for URL parameters), and outputs it to stdout.
-perl -pe 's/([^a-zA-Z0-9_.!~*()'\''-])/sprintf("%%%02X", ord($1))/ge'
+# Optionally, you can pass what's to be encoded as regular arguments
+# (no options exist, not even --help). Additionally, since shells seem to
+# like adding trailing newlines to everything, these are removed so this:
+#     encode-uri-component.sh 'foo bar' # or <<< 'foo bar'!
+# will result in "foo%20bar" and not "foo%20bar%0A".
+if [[ -n "$*" ]]; then
+	perl -pe 'chomp if eof;s/([^a-zA-Z0-9_.!~*()'\''-])/sprintf("%%%02X", ord($1))/ge' <<< "$*"
+else
+	perl -pe 'chomp if eof;s/([^a-zA-Z0-9_.!~*()'\''-])/sprintf("%%%02X", ord($1))/ge'
+fi
